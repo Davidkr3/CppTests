fa234a93c4f38c27d23cd75dfc035054ea27a1a6
David Cara <david.cara@ASDEBPFG000BNM.iris.cat>
2016-02-05 11:34:50 +0100

    c++ HelloWorld

---
+++ CppHelloWorld/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: david.cara
+ *
+ * Created on February 5, 2016, 11:24 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {    
+    cout << "Hello World!";    
+    printf("HOLA");
+    return 0;
+}
